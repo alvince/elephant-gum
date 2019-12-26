8fad8c636c7a9683a2d1e2bebef1d06b045b258e
alvince <alvince.zy@gmail.com>
2019-12-26 16:01:03 +0800

    Chore: publish config

--- property-loader/build.gradle.kts
+++ property-loader/build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     groovy
     kotlin("jvm")
+    `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "0.10.1"
 }
 
 sourceSets {
@@ -29,4 +31,22 @@ compileKotlin.kotlinOptions {
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
+}
+
+pluginBundle {
+    website = property("project.website").toString()
+    vcsUrl = property("project.website").toString()
+    tags = listOf("properties", "gradle")
+}
+
+gradlePlugin {
+    plugins {
+        create("propertiesLoaderPlugin") {
+            id = property("plugin.id").toString()
+            displayName = property("plugin.displayName").toString()
+            description = property("plugin.description").toString()
+            version = property("plugin.version").toString()
+            implementationClass = "cn.alvince.gradle.gum.properties.PropsLoaderPlugin"
+        }
+    }
 }
